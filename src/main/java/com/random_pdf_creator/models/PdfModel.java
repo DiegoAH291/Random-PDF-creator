2e493187a7d4836c80da537ee581d6b9a20344c1
DiegoAH291 <diegoaugustoh345@gmail.com>
2024-05-14 01:52:12 -0400

    integration of the function to create pdf

--- src/main/java/com/random_pdf_creator/models/PdfModel.java
+++ src/main/java/com/random_pdf_creator/models/PdfModel.java
@@ -1,6 +1,14 @@
 package com.random_pdf_creator.models;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class PdfModel {
 
@@ -21,11 +29,50 @@ public class PdfModel {
                 System.out.println("Error: the field is empty");
                 creator();
             } else {
-                System.out.println("PDF created successfully");
+
+                File folder = new File("files");
+                PDDocument document = new PDDocument();
+                PDPage page = new PDPage();
+                document.addPage(page);
+
+                if (!folder.exists()) {
+                    folder.mkdirs();
+                }
+
+                String filePath = folder.getAbsolutePath().concat("/").concat(title).concat(".pdf");
+
+                // Generate random text
+
+                byte leftLimit = 97;
+                byte rightLimit = 122;
+                short stringLength = 2000;
+
+                Random random = new Random();
+                StringBuilder buffer = new StringBuilder(stringLength);
+
+                for (int i = 0; i < stringLength; i++) {
+                    int randomLimited = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+                    buffer.append((char) randomLimited);
+                }
+
+                String randomInformation = buffer.toString();
+
+                try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                    contentStream.beginText();
+                    contentStream.setFont(PDType1Font.HELVETICA, 12);
+                    contentStream.newLineAtOffset(100, 700);
+                    contentStream.showText(randomInformation);
+                    contentStream.endText();
+                }
+
+                document.save(filePath);
+                document.close();
+                System.out.println("PDF created successfully at: " + filePath);
             }
 
-        } catch (Exception error) {
-            throw new Error(error.getMessage());
+        } catch (IOException error) {
+            error.printStackTrace();
+            throw new RuntimeException("Error creating PDF: " + error.getMessage());
         }
     }
 }
